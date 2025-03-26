04db000f58c75d6aaf61fa85739ab7893ee9d819
17070610 <schimea@hotmail.com>
2025-03-26 07:24:10 +0200

    Add the Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
 
 function App() {
   return (
     <div>
       <Navbar/>
+        <Shop/>
     </div>
   );
 }
