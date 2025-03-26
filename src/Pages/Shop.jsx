c215349799aa89d5f537f53e482511bbb39b3332
17070610 <schimea@hotmail.com>
2025-03-26 15:36:44 +0200

    Add a landing page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from "../Components/Hero/Hero";
 
 const Shop = () => {
     return (
         <div className="shop">
-
+            <Hero/>
         </div>
     )
 }
